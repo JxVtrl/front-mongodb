81de02286e40f8a6350b4ec8f3fe1ce677f7e0e1
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-12-10 20:19:13 -0300

    Add new models and images

---
+++ src/components/FaceIdWidget/index.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import * as faceApi from "face-api.js";
+import { useApp } from "@/contexts/contextApi";
+
+export default function FaceIdWidget() {
+  const { recognitionModal } = useApp();
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    alert("getUserMedia() não é suportado pelo seu navegador!");
+  }
+
+  const videoRef = useRef<HTMLVideoElement>(
+    document.getElementById("video") as HTMLVideoElement
+  );
+  const canvasRef = useRef<HTMLCanvasElement>(
+    document.getElementById("canvas") as HTMLCanvasElement
+  );
+  const [faceMatcher, setFaceMatcher] = useState<faceApi.FaceMatcher | null>(
+    null
+  );
+
+  // Carregar modelos de detecção facial
+  const loadModels = useCallback(async () => {
+    const MODEL_URL = "/models";
+    await faceApi.loadSsdMobilenetv1Model(MODEL_URL);
+    await faceApi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+    await faceApi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+    await faceApi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+    await faceApi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+  }, []);
+
+  // Inicializar a webcam
+  const startVideo = useCallback(async () => {
+    if (videoRef.current) {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+    }
+  }, []);
+
+  const transformDescriptors = useCallback(async (descriptors: any) => {
+    const float32Array = [];
+    for (let descriptor of descriptors) {
+      const result = await descriptor;
+      if (result) {
+        float32Array.push(new Float32Array(result.descriptor));
+      }
+    }
+    return float32Array;
+  }, []);
+
+  const createFaceMatcher = useCallback(async () => {
+    const facesMock = [
+      {
+        name: "João",
+        descriptors: [
+          "https://res.cloudinary.com/dppimfdxy/image/upload/v1702249589/teste/face_01/d6cotq9soibu0mo1xpeg.jpg",
+          "https://res.cloudinary.com/dppimfdxy/image/upload/v1702249590/teste/face_01/ciyzoasqglmcrx7vvdkx.jpg",
+          "https://res.cloudinary.com/dppimfdxy/image/upload/v1702249589/teste/face_01/igfguokymw8gu2h9kjxu.jpg",
+        ],
+      },
+    ];
+
+    const labeledFaceDescriptors = facesMock.map(async (face) => {
+      const descriptors = face.descriptors.map((url) => {
+        const img = new Image();
+        img.src = url;
+        return faceApi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+      });
+
+      console.log(descriptors);
+
+      const float32Array = await transformDescriptors(descriptors);
+
+      return new faceApi.LabeledFaceDescriptors(face.name, float32Array);
+    });
+
+    Promise.all(labeledFaceDescriptors).then((labeledFaceDescriptors) => {
+      if (labeledFaceDescriptors.length > 0) {
+        const faceMatcher = new faceApi.FaceMatcher(
+          labeledFaceDescriptors,
+          0.6
+        );
+        setFaceMatcher(faceMatcher);
+      }
+    });
+  }, [transformDescriptors]);
+
+  useEffect(() => {
+    console.log(faceMatcher);
+  }, [faceMatcher]);
+
+  useEffect(() => {
+    loadModels().then(() => {
+      createFaceMatcher();
+      startVideo();
+    });
+  }, [loadModels, createFaceMatcher, startVideo]);
+
+  if (!recognitionModal) return null;
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-[#00000050]">
+      <div className="bg-white shadow-lg rounded-lg flex justify-center h-[600px] w-[600px] p-6">
+        <div className="flex w-[300px] h-[300px] relative rounded-[50%]">
+          <video
+            id="video"
+            width="100%"
+            height="100%"
+            autoPlay
+            muted
+            ref={videoRef}
+          />
+          <canvas id="canvas" ref={canvasRef} className="absolute inset-0" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/contexts/contextApi.tsx
+++ src/contexts/contextApi.tsx
@@ -1,122 +1,128 @@
-"use client"
+"use client";
 
 import {
   criar_rotas,
   orderByYearThenByMonthThenByDayThenHour,
-} from "../utils/functions"
-import { Assento, Rota } from "../types"
-import { createContext, useContext, useState, useEffect } from "react"
-import axios from "axios"
+} from "../utils/functions";
+import { Assento, Rota } from "../types";
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 type User = {
-  name: string
-  email: string
-  password: string
-  confirmPassword: string
-  role: string
-  cpf?: string
-}
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  role: string;
+  cpf?: string;
+};
 
 interface ContextProps {
-  user: User | null
-  setUser: React.Dispatch<React.SetStateAction<User | null>>
-  rotas: Rota[]
-  selectedRoute: Rota | null
-  setSelectedRoute: React.Dispatch<React.SetStateAction<Rota | null>>
-  selectSeatModal: boolean
-  setSelectSeatModal: React.Dispatch<React.SetStateAction<boolean>>
-  seatsSelected: Assento[]
-  setSeatsSelected: React.Dispatch<React.SetStateAction<Assento[]>>
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  rotas: Rota[];
+  selectedRoute: Rota | null;
+  setSelectedRoute: React.Dispatch<React.SetStateAction<Rota | null>>;
+  selectSeatModal: boolean;
+  setSelectSeatModal: React.Dispatch<React.SetStateAction<boolean>>;
+  seatsSelected: Assento[];
+  setSeatsSelected: React.Dispatch<React.SetStateAction<Assento[]>>;
   passengersInfo: {
     passenger: {
-      name: string
-      cpf: string
-    }
+      name: string;
+      cpf: string;
+    };
     seat: {
-      numero: number
-    }
-  }[]
+      numero: number;
+    };
+  }[];
   setPassengersInfo: React.Dispatch<
     React.SetStateAction<
       {
         passenger: {
-          name: string
-          cpf: string
-        }
+          name: string;
+          cpf: string;
+        };
         seat: {
-          numero: number
-        }
+          numero: number;
+        };
       }[]
     >
-  >
-  checkoutStep: number
-  setCheckoutStep: React.Dispatch<React.SetStateAction<number>>
-  paymentType: "credit_card" | "pix" | "boleto"
+  >;
+  checkoutStep: number;
+  setCheckoutStep: React.Dispatch<React.SetStateAction<number>>;
+  paymentType: "credit_card" | "pix" | "boleto";
   setPaymentType: React.Dispatch<
     React.SetStateAction<"credit_card" | "pix" | "boleto">
-  >
-  modalAgradecimento: boolean
-  setModalAgradecimento: React.Dispatch<React.SetStateAction<boolean>>
-  compraRealizada: boolean
-  setCompraRealizada: React.Dispatch<React.SetStateAction<boolean>>
+  >;
+  modalAgradecimento: boolean;
+  setModalAgradecimento: React.Dispatch<React.SetStateAction<boolean>>;
+  compraRealizada: boolean;
+  setCompraRealizada: React.Dispatch<React.SetStateAction<boolean>>;
+  recognitionModal: boolean;
+  setRecognitionModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const AppContext = createContext<ContextProps>({} as ContextProps)
+const AppContext = createContext<ContextProps>({} as ContextProps);
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null)
-  const [rotas, setRotas] = useState<Rota[]>([])
-  const [selectedRoute, setSelectedRoute] = useState<Rota | null>(null)
-  const [selectSeatModal, setSelectSeatModal] = useState(false)
-  const [seatsSelected, setSeatsSelected] = useState<Assento[]>([])
-  const [checkoutStep, setCheckoutStep] = useState(0)
-  const [paymentType, setPaymentType] = useState<"credit_card" | "pix" | "boleto">('pix')
-  const [passengersInfo, setPassengersInfo] = useState<{
+  const [user, setUser] = useState<User | null>(null);
+  const [rotas, setRotas] = useState<Rota[]>([]);
+  const [selectedRoute, setSelectedRoute] = useState<Rota | null>(null);
+  const [selectSeatModal, setSelectSeatModal] = useState(false);
+  const [seatsSelected, setSeatsSelected] = useState<Assento[]>([]);
+  const [checkoutStep, setCheckoutStep] = useState(0);
+  const [paymentType, setPaymentType] = useState<
+    "credit_card" | "pix" | "boleto"
+  >("pix");
+  const [recognitionModal, setRecognitionModal] = useState(false);
+  const [passengersInfo, setPassengersInfo] = useState<
+    {
       passenger: {
-        name: string
-        cpf: string
-      }
+        name: string;
+        cpf: string;
+      };
       seat: {
-        numero: number
-      }
+        numero: number;
+      };
     }[]
-    >([])
-    const [modalAgradecimento, setModalAgradecimento] = useState(false);
+  >([]);
+  const [modalAgradecimento, setModalAgradecimento] = useState(false);
   const [compraRealizada, setCompraRealizada] = useState(false);
 
   const fetchRotas = async () => {
-    const rotas_local = localStorage.getItem("rotas")
+    const rotas_local = localStorage.getItem("rotas");
 
-    let used_routes = JSON.parse(rotas_local || "[]")
+    let used_routes = JSON.parse(rotas_local || "[]");
 
-    if (!rotas_local) used_routes = await criar_rotas()
-    localStorage.setItem("rotas", JSON.stringify(used_routes))
+    if (!rotas_local) used_routes = await criar_rotas();
+    localStorage.setItem("rotas", JSON.stringify(used_routes));
 
-    used_routes.sort(orderByYearThenByMonthThenByDayThenHour)
+    used_routes.sort(orderByYearThenByMonthThenByDayThenHour);
 
-    setRotas(used_routes)
-  }
+    setRotas(used_routes);
+  };
 
   const getRotas = async () => {
-    const url = `/api/rotas/all`
+    const url = `/api/rotas/all`;
 
-    const res = await axios.get(url)
-    console.log("Rotas disponiveis: " + res.data)
-    setRotas(res.data)
-  }
+    const res = await axios.get(url);
+    console.log("Rotas disponiveis: " + res.data);
+    setRotas(res.data);
+  };
 
   useEffect(() => {
     // getRotas()
-    fetchRotas()
-  }, [])
+    fetchRotas();
+  }, []);
 
   useEffect(() => {
-    const user = localStorage.getItem("user")
+    const user = localStorage.getItem("user");
 
     if (!!user && user !== "undefined") {
-      setUser(user.length > 0 ? JSON.parse(user) : null)
+      setUser(user.length > 0 ? JSON.parse(user) : null);
     }
-  }, [])
+  }, []);
 
   const value = {
     user,
@@ -130,13 +136,19 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     setSeatsSelected,
     passengersInfo,
     setPassengersInfo,
-    checkoutStep, setCheckoutStep,
-    paymentType, setPaymentType,
-    modalAgradecimento, setModalAgradecimento,
-    compraRealizada, setCompraRealizada
-  }
+    checkoutStep,
+    setCheckoutStep,
+    paymentType,
+    setPaymentType,
+    modalAgradecimento,
+    setModalAgradecimento,
+    compraRealizada,
+    setCompraRealizada,
+    recognitionModal,
+    setRecognitionModal,
+  };
 
-  return <AppContext.Provider value={value}>{children}</AppContext.Provider>
-}
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
 
-export const useApp = () => useContext(AppContext)
+export const useApp = () => useContext(AppContext);
