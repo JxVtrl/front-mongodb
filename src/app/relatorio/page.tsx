dcd393d5506653bb5ce027245ffceae99fcbf178
João Vitral <joaoviniciusvitral@hotmail.com>
2023-12-12 18:50:41 -0300

    Refactor download button styling

--- src/app/relatorio/page.tsx
+++ src/app/relatorio/page.tsx
@@ -26,7 +26,12 @@ const Page: React.FC = () => {
         p-4
 '>
         
-            <button onClick={downloadRouteContentExcel}>Download</button>
+        <button className='
+            bg-blue-500
+            hover:bg-blue-700
+            text-white font-bold py-2 px-4 rounded
+            mb-4
+' onClick={downloadRouteContentExcel}>Download</button>
         
         
         
